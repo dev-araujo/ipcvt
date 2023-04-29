2ee11be238d8534a8daeec5c5f6fd2ac6536629f
dev-araujo <theriancirculo@protonmail.com>
2023-04-29 11:41:24 -0300

    style: carousel test

--- church-project/src/views/home/index.jsx
+++ church-project/src/views/home/index.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
+import Carousel from "react-elastic-carousel";
 
 import { BASE_URL } from "../../service/BASE_URL";
 
@@ -27,26 +28,28 @@ const Home = () => {
 
   return (
     <section className="container-videos-area">
-      {dataBase.map((video, index) => {
-        return (
-          <section key={index} className="videos-area">
-            <div className="layout-videos">
-              <h2 className="title-videos">{video.snippet.title}</h2>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
-              >
-                <img
-                  alt="video thumbnail"
-                  className="thumb"
-                  src={video.snippet.thumbnails.high.url}
-                />
-              </a>
-            </div>
-          </section>
-        );
-      })}
+      <Carousel itemsToShow={2}>
+        {dataBase.map((video, index) => {
+          return (
+            <section key={index} className="videos-area">
+              <div className="layout-videos">
+                <h2 className="title-videos">{video.snippet.title}</h2>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+                >
+                  <img
+                    alt="video thumbnail"
+                    className="thumb"
+                    src={video.snippet.thumbnails.high.url}
+                  />
+                </a>
+              </div>
+            </section>
+          );
+        })}
+      </Carousel>
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={load}
