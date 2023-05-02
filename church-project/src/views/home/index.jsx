b259344b1c7e70fa3efb2a7c5430ebd0d4b6e6ce
dev-araujo <theriancirculo@protonmail.com>
2023-05-02 14:56:47 -0300

    chore: fix lib for icons

--- church-project/src/views/home/index.jsx
+++ church-project/src/views/home/index.jsx
@@ -4,10 +4,10 @@ import axios from "axios";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 import Carousel from "react-elastic-carousel";
-import { Icon } from '@iconify/react';
-import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import InstagramIcon from '@mui/icons-material/Instagram';
+// import { Icon } from "@iconify/react";
+// import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+// import YouTubeIcon from "@mui/icons-material/YouTube";
+// import InstagramIcon from "@mui/icons-material/Instagram";
 
 import { BASE_URL } from "../../service/BASE_URL";
 
@@ -31,57 +31,58 @@ const Home = () => {
   }, []);
 
   return (
-  <>
-    <section className='social-media' >
-      <p>siga-nos em nossas redes sociais</p>
-      <div className='icons' >
-        <FacebookTwoToneIcon className='facebook-icon' />
-        <YouTubeIcon className='youtube-icon' />
-        <InstagramIcon className='instagram-icon' />
-        <Icon icon="mdi:spotify" className='spotify-icon' />
-      </div>
-    </section>
-    <section className="container-videos-area">
-      <Carousel itemsToShow={1}>
-        {dataBase.map((video, index) => {
-          return (
-            <section key={index} className="videos-area">
-              <div className="layout-videos">
-                <h2 className="title-videos">{video.snippet.title}</h2>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
-                >
-                  <img
-                    alt="video thumbnail"
-                    className="thumb"
-                    src={video.snippet.thumbnails.high.url}
-                  />
-                </a>
-              </div>
-            </section>
-          );
-        })}
-      </Carousel>
-
-      <footer>
-        <div className='footer-content' >
-          <p>ipcvt - Igreja Presbiteriana Central em Vilar dos Teles  - Rua Venância Oliveira Santos,
-             289 - Vilar dos Teles  - CEP 25560-000  São João de  Meriti/RJ 
-          </p>
+    <>
+      <section className="social-media">
+        <p>siga-nos em nossas redes sociais</p>
+        <div className="icons">
+          <i class="bi bi-facebook"></i>
+          <i class="bi bi-youtube"></i>
+          <i class="bi bi-instagram"></i>
+          <i class="bi bi-spotify"></i>
         </div>
-      </footer>
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={load}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    </section>
-  </>
+      </section>
+      <section className="container-videos-area">
+        <Carousel itemsToShow={1}>
+          {dataBase.map((video, index) => {
+            return (
+              <section key={index} className="videos-area">
+                <div className="layout-videos">
+                  <h2 className="title-videos">{video.snippet.title}</h2>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+                  >
+                    <img
+                      alt="video thumbnail"
+                      className="thumb"
+                      src={video.snippet.thumbnails.high.url}
+                    />
+                  </a>
+                </div>
+              </section>
+            );
+          })}
+        </Carousel>
+
+        <footer>
+          <div className="footer-content">
+            <p>
+              ipcvt - Igreja Presbiteriana Central em Vilar dos Teles - Rua
+              Venância Oliveira Santos, 289 - Vilar dos Teles - CEP 25560-000
+              São João de Meriti/RJ
+            </p>
+          </div>
+        </footer>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={load}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </section>
+    </>
   );
 };
 
 export default Home;
-
