10688e9c3f6c7f2034a940d47c78961e908a9363
dev-araujo <theriancirculo@protonmail.com>
2023-05-03 12:05:34 -0300

    style: info

--- church-project/src/components/Footer/index.jsx
+++ church-project/src/components/Footer/index.jsx
@@ -5,32 +5,56 @@ import "./styles.css";
 
 function Footer() {
   const nav = useNavigate();
+  const contentIcons = [
+    { class: "bi bi-facebook", link: "https://pt-br.facebook.com/ipcvt/" },
+    {
+      class: "bi bi-youtube",
+      link: "https://www.youtube.com/@IPCVTIgrejaPresbiteriana",
+    },
+    { class: "bi bi-instagram", link: "https://www.instagram.com/ipcvt/" },
+    {
+      class: "bi bi-spotify",
+      link: "https://open.spotify.com/show/1RWZu10Ghrz8AlRaLS3imu",
+    },
+  ];
 
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <img
-          className="footer__logo"
-          onClick={() => nav("/")}
-          src={ImageLogo}
-          alt="logo-ipcvt"
-        />
-        <p>
-          ipcvt - Igreja Presbiteriana Central em Vilar dos Teles - Rua Venância
-          Oliveira Santos, 289 - Vilar dos Teles - CEP 25560-000 São João de
-          Meriti/RJ
-        </p>
-      </div>
-
-      <section className="social-media">
-        <p>siga-nos em nossas redes sociais</p>
-        <div className="icons">
-          <i className="bi bi-facebook"></i>
-          <i className="bi bi-youtube"></i>
-          <i className="bi bi-instagram"></i>
-          <i className="bi bi-spotify"></i>
+      <div className="footer__container">
+        <div className="footer-content">
+          <img
+            className="footer__logo"
+            onClick={() => nav("/")}
+            src={ImageLogo}
+            alt="logo-ipcvt"
+          />
+          <p>
+            ipcvt - Igreja Presbiteriana Central em Vilar dos Teles - Rua
+            Venância Oliveira Santos, 289 - Vilar dos Teles - CEP 25560-000 São
+            João de Meriti/RJ
+          </p>
         </div>
-      </section>
+
+        <section className="activities">
+          <p className="title">Atividades </p>
+          <div className="activies__list">
+            <p>Domingo -09:00 - EBD (presencial)</p>
+            <p>Domingo -18:00 - Culto (presencial e online)</p>
+            <p>Quarta - 19:30 - Reencontro (presencial)</p>
+          </div>
+        </section>
+
+        <section className="social-media">
+          <p>Redes sociais</p>
+          <div className="icons">
+            {contentIcons.map((icon) => (
+              <a href={icon.link} target="_blank" rel="noreferrer">
+                <i className={icon.class}></i>
+              </a>
+            ))}
+          </div>
+        </section>
+      </div>
     </footer>
   );
 }
